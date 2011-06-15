b398343c766529629b496051102cc5b609d2b5d9
Justis Durkee <justis.durkee@gmail.com>
2011-06-15 01:09:18 -0700

    Rename.

--- fb/hoppity/php/hoppity.php
+++
@@ -1,6 +0,0 @@
-<?php
-for ($i = 1; $i <= file_get_contents($argv[1]); $i++) {
-  if     ($i % (3*5) == 0) { print "Hop\n"; }
-  elseif ($i % 3     == 0) { print "Hoppity\n"; }
-  elseif ($i % 5     == 0) { print "Hophop\n"; }
-}
