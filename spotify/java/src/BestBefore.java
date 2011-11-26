6c96255ef65795e4b633ef6fb3843e16546ee577
Justis Durkee <justis.durkee@gmail.com>
2011-11-25 21:17:15 -0800

    Remove unused import.

--- spotify/java/src/BestBefore.java
+++ spotify/java/src/BestBefore.java
@@ -5,7 +5,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
